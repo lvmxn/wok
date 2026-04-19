3d81e1da0517b7749fe63582609cd6a0c79d6cba
lvmxn <lukanindan@gmail.com>
2026-04-19 19:57:10 +0500

    Added 'now' function

--- helpers.py
+++ helpers.py
@@ -33,10 +33,13 @@ def translate(word):
     result = ts.translate_text(query_text, translator='bing', from_language='en', to_language='ru')
     return(result)
 
+def now():
+    return date.today().strftime("%Y-%m-%d")
+
 def start(db, user_id):
     db.execute("""
         INSERT INTO user_words (user_id, next_review, word_id)
         SELECT ?, ?, id FROM words 
         ORDER BY RANDOM() 
         LIMIT ?
-    """, user_id, date.today().strftime("%Y-%m-%d"), RANDOM_WORD_START_COUNT)
+    """, user_id, now(), RANDOM_WORD_START_COUNT)
